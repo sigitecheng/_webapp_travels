57e8b595a53dd2e8ef2e62e2bef77bce2f3b7ca0
Sigimatika <sigitechindo@gmail.com>
2023-09-03 08:58:29 +0700

    intal baru

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// import { useState } from 'react'
+const App = () => {
+  return(
+    <>
+    <Header />  
+    <Home />  
+    <Main />  
+    <Footer />  
+    <Header />  
+
+    </>
+  )
+}
+
+export default App
