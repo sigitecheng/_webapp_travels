eb4651e1b94702e187e319356b14bc573bdef478
Sigimatika <sigitechindo@gmail.com>
2023-09-03 22:22:47 +0700

    tambah lagi data yang ada

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,36 +1,65 @@
 // import React from 'react'
 import './navbar.css'
 import { GiTeePipe } from 'react-icons/gi'
+import {AiFillCloseCircle} from 'react-icons/ai'
+import {PiDotsNineBold} from 'react-icons/pi'
+// import {CgLogIn} from 'react-icons/cg'
 
 const navbar = () => {
     return(
         <section className='navbarSection'> 
             <header className='header flex'>
-
                 <div className='logoDiv'>
-                    <a href="#navbar" className='logo'>
-                    <h1><GiTeePipe className='icon' />  Sigitmatika Air Solutions</h1>
+                    <a href="#navbar" className='logo flex'>
+                    <h1><GiTeePipe className='icon' />  Travel</h1>
                     {/* <h1>Sigitmatika Air Solutions</h1> */}
                     </a>
                 </div>
 
-                {/* <div className='navbar'>
-                    <ul className='navLists flex'>
-                        <li className='navItem'> 
-                        <a href="#" className='navlink'>Home</a>
+                <div className='navBar'>
+                    <ul className='navlists flex'>
+                        <li className='navitem '>
+                            <a href="" className='navLink'>Home</a>
                         </li>
-                        <li className='navItem'> 
-                        <a href="#" className='navlink'>Home</a>
-                        </li>
-                        <li className='navItem'> 
-                        <a href="#" className='navlink'>Home</a>
-                        </li>
-                        <li className='navItem'> 
-                        <a href="#" className='navlink'>Home</a>
-                        </li>
-                    </ul>
-                </div> */}
 
+                        <li className='navitem'>
+                            <a href="" className='navLink'>Packages</a>
+                        </li>
+
+                        <li className='navitem'>
+                            <a href="" className='navLink'>Shop</a>
+                        </li>
+
+                        <li className='navitem'>
+                            <a href="" className='navLink'>About</a>
+                        </li>
+
+                        <li className='navitem'>
+                            <a href="" className='navLink'>Pages</a>
+                        </li>
+
+                        <li className='navitem'>
+                            <a href="" className='navLink'>News</a>
+                        </li>
+
+                        <li className='navitem'>
+                            <a href="" className='navLink'>Contacts</a>
+                        </li>
+
+                        <button className='btn'>
+                            <a href="#">LOGIN</a>
+                        </button>
+                    </ul>
+                </div>
+
+
+                <div className='navBar'>
+                        <AiFillCloseCircle className='icon'/>
+                    </div>
+
+                    <div className="togglenavbar">
+                        <PiDotsNineBold className='icon'/>
+                    </div>
             </header>
         </section>
     )
