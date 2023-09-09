838aba518174791704d8c8309ea708af0002f446
Sigimatika <sigitechindo@gmail.com>
2023-09-09 16:33:26 +0700

    pembaruan baru

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,19 @@
 // import { useState } from 'react'
 
-// import Navbar from './components/Navbar/Navbar'
-// import Home from './components/Home/Home'
-// import Mains from './components/Mains/Mains'
-// import Footer from './components/Footer/Footer'
+import Navbar from './components/Navbar/Navbar'
+import Home from './components/Home/Home'
+import Mains from './components/Mains/Mains'
+import Footer from './components/Footer/Footer'
 // import Icons from './components/icons'
 
 
 export default function App() {
   return(
     <>
-    {/* <Navbar />   */}
-    {/* <Home />   */}
-    {/* <Mains />   */}
-    {/* <Footer />   */}
-
-  <section>
-    <div>
-      <h1>Ini adalah halaman diriku</h1>
-    </div>
-  </section>
+    <Navbar />  
+    <Home />  
+    <Mains />  
+    <Footer />  
     </>
   )
 }
